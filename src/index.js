89e3cf1bd9297a5cba673eae7cc81b688f5f605b
Manas Gautam <gautammanas2000@gmail.com>
2021-08-14 13:35:25 +0530

    1330px commit 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './laptop-13.css';
 import './tablet.css';
 import './mobile.css';
 import App from './App';
