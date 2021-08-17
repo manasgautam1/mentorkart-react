f569b015f926e8151dede6ab09046ad46e160391
Manas Gautam <gautammanas2000@gmail.com>
2021-08-17 22:21:07 +0530

    last commit

--- src/components/showcase-section/Facilities.js
+++ src/components/showcase-section/Facilities.js
@@ -4,12 +4,12 @@ import Facility from './Facility';
 const Facilities = () => {
   return (
     <div className='facilities row text-center my-4'>
-      <Facility text='Live Session with Mentors' />
-      <Facility text='Schedule Calls with Mentors' />
-      <Facility text='1:1 Chat with Mentor' />
-      <Facility text='Ask a Mentor (Group Chat)' />
-      <Facility text='Goals Setting' />
-      <Facility text='Master Classes' />
+      <Facility img='/images/icon1.png' text='Live Session with Mentors' />
+      <Facility img='/images/icon2.png' text='Schedule Calls with Mentors' />
+      <Facility img='/images/icon3.png' text='1:1 Chat with Mentor' />
+      <Facility img='/images/icon4.png' text='Ask a Mentor (Group Chat)' />
+      <Facility img='/images/icon5.png' text='Goals Setting' />
+      <Facility img='/images/icon6.png' text='Master Classes' />
     </div>
   );
 };
--- src/components/showcase-section/Facility.js
+++ src/components/showcase-section/Facility.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Facility = (props) => {
   return (
-    <div className='facility scale-animation col-md-2 col-4 px-md-2 p-0'>
-      <img className='facility-img' src='/images/facility-img.png' alt='' />
+    <div className='facility scale-animation-2 col-md-2 col-4 px-md-2 p-0'>
+      <img className='facility-img' src={props.img} alt='' />
       <p className='facility-text px-lg-3 px-md-1 px-md-0 px-2'>{props.text}</p>
     </div>
   );
