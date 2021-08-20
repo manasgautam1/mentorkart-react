b415a1320f5c40563b7adb9d30dcbeca18731356
Manas Gautam <gautammanas2000@gmail.com>
2021-08-20 14:16:52 +0530

    facility image changes

--- src/components/showcase-section/Facility.js
+++ src/components/showcase-section/Facility.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Facility = (props) => {
   return (
     <div className='facility scale-animation-2 col-md-2 col-4 px-md-2 p-0'>
-      <img className='facility-img' src={props.img} alt='' />
+      <div className='facility-img'>
+        <img src={props.img} alt='' />
+      </div>
       <p className='facility-text px-lg-3 px-md-1 px-md-0 px-2'>{props.text}</p>
     </div>
   );
